dee735f85c49125765dd70236b3225499dc2d91a
yuanzui-cf <im.leojia@gmail.com>
2026-02-14 23:42:12 +0800

    feat: Add tool parameter metadata to Tools

--- src/pkgs/adapter/tools.ts
+++ src/pkgs/adapter/tools.ts
@@ -1,6 +1,12 @@
+export interface ToolParameters {
+  [key: string]: any;
+}
+
 export default abstract class Tools {
   abstract readonly name: string;
   abstract readonly description: string;
+  abstract readonly parameters?: ToolParameters;
+  abstract readonly required?: string[];
 
   abstract call(...args: any[]): Promise<string>;
 }
