4d14c770b2f6e4973b203f714664fb57df8fc92f
yuanzui-cf <im.leojia@gmail.com>
2026-02-17 14:04:14 +0800

    refactor(tools): Add generic types to Tools call signature

--- src/sunflower/tools/index.ts
+++ src/sunflower/tools/index.ts
@@ -2,11 +2,11 @@ export interface ToolParameters {
   [key: string]: any;
 }
 
-export default abstract class Tools {
+export default abstract class Tools<T = any, P = string> {
   abstract readonly name: string;
   abstract readonly description: string;
   abstract readonly parameters?: ToolParameters;
   abstract readonly required?: string[];
 
-  abstract call(...args: any[]): Promise<string>;
+  abstract call(args: T): Promise<P>;
 }
