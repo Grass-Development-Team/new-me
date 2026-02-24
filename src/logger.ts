8f7eadec5d9bd151cca0e198d4c7f9abb97aba60
yuanzui-cf <im.leojia@gmail.com>
2026-02-24 19:30:38 +0800

    refactor(logger): Use JSON logger format

--- src/logger.ts
+++ src/logger.ts
@@ -4,20 +4,8 @@ const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   format: winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.printf(({ level, message, timestamp, ...rest }) => {
-      // 处理多参数情况
-      const messages = Array.isArray(message) ? message : [message];
-      const otherArgs = Object.keys(rest).length ? [JSON.stringify(rest)] : [];
-      const allMessages = [...messages, ...otherArgs].join(" ");
-
-      // 给日期和级别添加颜色
-      const colorizer = winston.format.colorize();
-      const coloredLevel = colorizer.colorize(level, level.toUpperCase());
-      const coloredTimestamp = colorizer.colorize(level, `${timestamp}`);
-
-      // 设置格式: [时间戳] [级别] 消息
-      return `[${coloredTimestamp}] [${coloredLevel}] ${allMessages}`;
-    }),
+    winston.format.errors({ stack: true }),
+    winston.format.json(),
   ),
   transports: [new winston.transports.Console()],
 });
